7f5cc6c9bdd067e8e4f2ab48b2f3d24e181514e5
OLIWIERKARDYNAL-STUD <G00434405@atu.ie>
2026-02-18 16:29:42 +0000

    Fixed max age

--- src/main/java/com/example/demo/service/CalculatorService.java
+++ src/main/java/com/example/demo/service/CalculatorService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
         public int divide(int a, int b) {
             return a / b;
         }
+
         public String input(String name, int age) {
             return "Hello, my name is " + name + " and i am " + age + " years old";
         }
